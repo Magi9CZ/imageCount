cb8ca1b9ab3dd487105d4609a584d23c2a65c15c
Michal Jiránek <michal.jiranek00@gmail.com>
2023-11-27 18:22:22 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(props) {
+
+    function handleInput(e) {
+        setUserInput(e.target.value);
+    }
+
+    function passData() {
+        checkAnswer();
+        props.onChange(userInput);
+    }
+
+    const [userInput, setUserInput] = useState("");
+
+    useEffect(() => {
+        passData();
+    }, [userInput, 1]);
+
+    const images = props.data.images;
+    const priklady = props.data.priklady;
+    const question = props.data.question;
+    const result = props.data.result;
+    function getCorrectMark(mark) {
+        switch (mark){
+            case 'minus':
+                return '-'
+            case undefined:
+                return <div className="item-mark">=</div>
+            default:
+                return '+'
+        }
+    }
+
+    function checkAnswer() {
+        if (userInput == result){
+            console.log("správně");
+            props.checkAnswer(true);
+        }else{
+            console.log("špatně");
+            props.checkAnswer(false);
+        }
+    }
+
+
+    if (props.readOnly === true) {
+        return (
+            <div className="App">
+                {priklady.map(item =>
+                    <div className="item-container">
+                        {[...Array(item.count).keys()].map((img, index) =>
+                            <div className="item-image">
+                                <img src={images[item.img].src}></img>
+                                {(index + 1 < item.count) ? <div className="item-mark">+</div> :
+                                    <div className="item-mark">=</div>}
+                            </div>
+                        )}
+                        <div className="item-result">{item.result}</div>
+                    </div>)}
+                <div className="result">
+                    {question.map(item =>
+                        <div className="item-container">
+                            <div className="item-image">
+                                <img src={images[item.img].src}></img>
+                                <div className="item-mark">{getCorrectMark(item.mark)}</div>
+                            </div>
+                        </div>)}
+                    <input onChange={handleInput}/>
+                </div>
+                <button onClick={checkAnswer}>Zkontrolovat</button>
+            </div>
+        );
+    }else{
+        return (
+            <div className="App">
+                {priklady.map(item =>
+                    <div className="item-container">
+                        {[...Array(item.count).keys()].map((img, index) =>
+                            <div className="item-image">
+                                <img src={images[item.img].src}></img>
+                                {(index + 1 < item.count) ? <div className="item-mark">+</div> :
+                                    <div className="item-mark">=</div>}
+                            </div>
+                        )}
+                        <div className="item-result">{item.result}</div>
+                    </div>)}
+                <div className="result">
+                    {question.map(item =>
+                        <div className="item-container">
+                            <div className="item-image">
+                                <img src={images[item.img].src}></img>
+                                <div className="item-mark">{getCorrectMark(item.mark)}</div>
+                            </div>
+                        </div>)}
+                    <h3>{props.restoreData.userInput}</h3>
+                </div>
+            </div>
+            );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,95 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Question_imageCount() {
+
+    let playable = true;
+    let userInput;
+    let completed = false;
+
+    Question_imageCount.prototype.init = function (questionKey, location, config) {
+        this.questionKey = questionKey;
+        this.location = location;
+        this.config = config;
+
+        var self = this;
+
+
+        //init save & cancel answer handlers pro tlačítka
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        console.log(config);
+        root.render(
+            <React.StrictMode>
+                <App data={config} onChange={stavHry} readOnly={playable} checkAnswer={answerCheck}/>
+            </React.StrictMode>
+        );
+    };
+
+    function answerCheck(e) {
+        if (e === true){
+            completed = true;
+        }
+    }
+
+    function stavHry(e) {
+        userInput = e;
+        checkState();
+    }
+
+    function checkState() {
+        console.log("stav user: ", userInput);
+    }
+
+    Question_imageCount.prototype.onlyShowInit = function (questionKey, location, config) {
+        this.questionKey = questionKey;
+        this.location = location;
+        this.config = config;
+        playable = false;
+        console.log("play " + playable);
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        console.log(config);
+        root.render(
+            <React.StrictMode>
+                <App data={config} userData={userInput} readOnly={playable}/>
+            </React.StrictMode>
+        );
+    };
+
+    function saveAnswer(odp) {
+        userInput = odp;
+        console.log("správně?: " + completed);
+    }
+
+    Question_imageCount.prototype.answer = function () {
+        const odpoved = {userInput, completed};
+        return odpoved;
+    };
+
+    /**
+     * Returns data for recovery
+     * @returns {string}
+     */
+    Question_imageCount.prototype.recoveryData = function () {
+        const stav = {userInput, completed};
+        return stav;
+    };
+
+    /**
+     * Restore application and disable moving
+     * @param data
+     */
+    Question_imageCount.prototype.restore = function (data) {
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        console.log(config);
+        root.render(
+            <React.StrictMode>
+                <App data={config} restoreData={data} onChange={stavHry} readOnly={playable} checkAnswer={answerCheck}/>
+            </React.StrictMode>
+        );
+
+    };
+}
+export default Question_imageCount;
